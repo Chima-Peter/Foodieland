bf9f1f8adc51fa1c477aeb4e7b516a61c46b8ce2
Mobi <chimaojimma@gmail.com>
2024-07-30 20:24:23 +0100

    Create categories

--- src/components/ui/home.tsx
+++ src/components/ui/home.tsx
@@ -7,6 +7,35 @@ import { FaPlayCircle } from "react-icons/fa";
 
 function Home() {
    const base = useBaseContext()
+   // Access basename using context from APP.tsx
+
+   const categories = [
+      {
+         name: 'Breakfast',
+         gradient: 'gray-200'
+      },
+      {
+         name: 'Vegan',
+         gradient: 'green-200'
+      },
+      {
+         name: 'Meat',
+         gradient: 'red-200'
+      },
+      {
+         name: 'Dessert',
+         gradient: 'pink-100'
+      },
+      {
+         name: 'Lunch',
+         gradient: 'gray-200'
+      },
+      {
+         name: 'Chocolate',
+         gradient: 'gray-200'
+      },
+   ]
+   // Array to render categories
 
   return (
    <main className='flex flex-col gap-8 box-border overflow-hidden'>
@@ -14,11 +43,12 @@ function Home() {
       <MediaQuery minWidth={768}>
             <DesktopNav />
       </MediaQuery>
-      <section className="w-[100%] px-6">
+      <section className="w-[100%] px-6 flex flex-col gap-14">
+         {/* MAIN IMAGE */}
          <div className="bg-[#E7FAFE] flex rounded-3xl relative justify-between md:flex-row flex-col flex-1">
             <div className="font-nav flex flex-col items-start shrink gap-6 px-6 py-14 w-fit">
                <div className="px-4 py-3 flex w-fit shadow-xl items-center gap-2 rounded-3xl bg-white">
-                  <img src= {`${base}images/all/img1.png`} className="w-5 h-5" alt="recipes" />
+                  <img src= {`${base}images/main/img1.png`} className="w-5 h-5" alt="recipes" />
                   <p className="text-sm font-[600]">
                      Hot recipes
                   </p>
@@ -50,7 +80,7 @@ function Home() {
                </div>
                <div className="flex items-center justify-between w-[100%] mt-8 flex-wrap gap-4">
                   <div className="flex items-center gap-2">
-                     <img src={`${base}images/all/user.png`} alt="user" className="w-[50px] h-[50px] rounded-full" />
+                     <img src={`${base}images/main/user.png`} alt="user" className="w-[50px] h-[50px] rounded-full" />
                      <div className="flex flex-col gap-2">
                         <h2 className="font-[700] text-[16px] leading-[19px] tracking-[-2%]">
                            John Smith
@@ -71,7 +101,7 @@ function Home() {
             <div className="absolute md:translate-x-[-450px] lg:translate-x-[-520px] right-0 pt-6 hidden md:block shrink-0">
                <div className="bg-black rounded-[50%] w-[150px] h-[150px] flex justify-center items-center">
                   <div className="bg-white rounded-[50%] w-[80px] h-[80px] flex justify-center items-center">
-                     <img src={`${base}images/all/thumb.png`} alt="thumbs up" className="w-[50px] -rotate-[25deg] h-[50px]" />
+                     <img src={`${base}images/main/thumb.png`} alt="thumbs up" className="w-[50px] -rotate-[25deg] h-[50px]" />
                   </div>
                </div>
             </div>
@@ -82,6 +112,29 @@ function Home() {
                alt="ERROR PAGE" 
                rel='preload' 
                className="rounded-e-3xl rounded-r-3xl shrink-0"/>
+         </div>
+         {/* CATEGORIES PAGE */}
+         <div className="font-nav w-[100%]">
+            <div className="flex w-[100%] justify-between">
+               <h3 className="font-[600] text-[24px] leading-[58px] tarcking-[-4%]">
+                  Categories
+               </h3>
+               <button type="button" className="font-[600] text-[12px] leading-[19px] tracking-[-2% bg-[#E7FAFE] w-fit px-6 rounded-xl">
+                  View All Categories
+               </button>
+            </div>
+            <div className="flex w-[100%] flex-wrap gap-2 md:justify-between">
+               {
+                  categories.map(value => (
+                     <div key={value.name} className={`flex flex-col gap-2 items-center w-fit p-6 pb-3 text-center bg-gradient-to-b from-white to-${value.gradient} rounded-b-xl`}>
+                        <img src={`${base}images/categories/${value.name}.png`} alt="" className="w-[50px] h-[50px]"/>
+                        <p className="font-[600] text-[12px] leading-[22px] tracking-[-2%]">
+                           {value.name}
+                        </p>
+                     </div>
+                  ))
+               }
+            </div>
          </div>
       </section>
    </main>
